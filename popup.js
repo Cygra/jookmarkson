be07dd3d2d3513e50e99618c26ad7b3987f9c354
Cygra <sjtuwbh@gmail.com>
2019-04-03 17:22:10 +0800

    人面不知何处去，桃花依旧笑春风。

--- popup.js
+++ popup.js
@@ -3,7 +3,7 @@ var exportBtn = document.getElementById("export");
 exportBtn.onclick = function() {
   chrome.bookmarks.getTree(function(items) {
     chrome.downloads.download({
-      url: "data:application/json," + escape(JSON.stringify(items)),
+      url: "data:application/json," + escape(JSON.stringify(items, null, 2)),
       filename: 'bookmarks.json'
     });
   });
